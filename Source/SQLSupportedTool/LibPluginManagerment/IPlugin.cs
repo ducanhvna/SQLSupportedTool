aa9fc392502e5f0a78f9838d92ff37e8a4abc1f1
Nguyễn Đức Anh <ducanhvna@outlook.com>
2018-07-27 10:31:13 +0700

    Add Plugin Managerment Project

---
+++ Source/SQLSupportedTool/LibPluginManagerment/IPlugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginManagermentNS
+{
+    /// <summary>
+    /// Plugin Interface
+    /// </summary>
+    public interface IPlugin
+    {
+        /// <summary>
+        /// Name of Plugin
+        /// </summary>
+        string Name { get; }
+
+        void Do();
+
+    }
+}
